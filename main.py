50f9551a5e2cfa3c86c1b199fd22041bb1754d35
PoliNetwork <44198123+PoliNetwork@users.noreply.github.com>
2021-07-13 09:32:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 # imports
 import os
+import time
 import traceback
 import urllib.request
 from bs4 import BeautifulSoup
+import datetime
 
 try:
     from urllib.parse import urlparse, urljoin
 except ImportError:
-    from urlparse import urlparse
+    try:
+        from urlparse import urlparse
+    except Exception as e_import:
+        print(e_import)
 
 # variables
 to_download = []
 success_download = 0
+global url_global
 
 
 # methods
@@ -27,7 +33,7 @@ def addLink(link):
     to_download.append(elem)
 
 
-def filterLink(soup, i, url):
+def filterLink(soup, url):
     url = str(url)
 
     new_soup = BeautifulSoup('<link rel="stylesheet" href="%s" type="text/css">' % '../style/ateneo2014.css.jsp',
@@ -39,6 +45,9 @@ def filterLink(soup, i, url):
     new_soup3 = BeautifulSoup('<link rel="stylesheet" href="%s" type="text/css">' % '../style/graduatorie.css',
                               features="html.parser")
     soup.head.insert(0, new_soup3)
+    new_soup4 = BeautifulSoup("<meta charset='UTF-8'>",
+                              features="html.parser")
+    soup.head.insert(0, new_soup4)
 
     if url.endswith("_generale.html"):
         soup.select_one(".titolo").decompose()
@@ -71,15 +80,41 @@ def filterLink(soup, i, url):
     pass
 
 
-def downloadAndAddChildrenUrl1(i, start):
+def getCorso(soup):
+    if soup is None:
+        return None
+
+    item = soup.select(".intestazione")[2]
+    return item
+
+    pass
+
+
+def getFase(soup):
+    if soup is None:
+        return None
+
+    item = soup.select(".intestazione")[3]
+    return item
+    pass
+
+
+def downloadAndAddChildrenUrl1(i, start, i_url):
+    global url_global
+    global success_download
+
     elem = to_download[i]
     url = elem["url"]
     try:
         soup = BeautifulSoup(urllib.request.urlopen(url), features="html.parser")
-        soup = filterLink(soup, i, url)
+        soup = filterLink(soup, url)
         to_download[i]["content"] = soup
+        if i == 0:
+            url_global[i_url]["corso"] = getCorso(soup)
+            url_global[i_url]["fase"] = getFase(soup)
+
         if soup is not None:
-            success_download = success_download + 1
+            success_download += 1
             for link in soup.find_all('a', href=True):
                 link = urljoin(url, link["href"])
                 if link.startswith(start):
@@ -92,9 +127,7 @@ def downloadAndAddChildrenUrl1(i, start):
     pass
 
 
-def directoryOutput(i, base_output, start_len):
-    url = str(to_download[i]["url"])
-
+def directoryOutput(url, base_output, start_len, return_first_folder):
     url2 = url[start_len:]
     url3 = url2.split("/")
 
@@ -111,6 +144,10 @@ def directoryOutput(i, base_output, start_len):
     j = 0
     while j < (len(url3) - 1):
         path = path + "/" + url3[j]
+
+        if return_first_folder:
+            return path, url3[0]
+
         if os.path.isdir(path):
             pass
         else:
@@ -118,24 +155,26 @@ def directoryOutput(i, base_output, start_len):
 
         j = j + 1
 
-    return path + "/" + url3[len(url3) - 1]
+    return path + "/" + url3[len(url3) - 1], url3[0]
 
 
-def downloadAndAddChildrenUrl(url, start):
+def downloadAndAddChildrenUrl(url, start, base_output, i_url, only_first):
     elem = {"url": url}
     to_download.append(elem)
 
     i = 0
-    while i < len(to_download):
-        downloadAndAddChildrenUrl1(i, start)
-        i = i + 1
-
-    base_output = "D:\\git\\Polimi\\polinetwork.github.io\\graduatorie"
+    if not only_first:
+        while i < len(to_download):
+            downloadAndAddChildrenUrl1(i, start, i_url)
+            i = i + 1
+    else:
+        downloadAndAddChildrenUrl1(i, start, i_url)
 
     start_len = len(start)
     i = 0
     while i < len(to_download):
-        path = directoryOutput(i, base_output, start_len)
+        url2 = str(to_download[i]["url"])
+        path, path_first = directoryOutput(url2, base_output, start_len, return_first_folder=False)
         file_to_write = None
         try:
             file_to_write = to_download[i]["content"]
@@ -152,47 +191,156 @@ def downloadAndAddChildrenUrl(url, start):
         pass
 
 
+def executeDownload(url, i, start, base_output, only_first):
+    global to_download
+    global success_download
+
+    time.sleep(3)
+
+    to_download = []
+    success_download = 0
+    try:
+        downloadAndAddChildrenUrl(url[i]["url"], start, base_output, i, only_first)
+        if success_download > 0:
+            print("Done [" + url[i]["url"] + "]")
+            return 1
+        else:
+            print("Error [" + url[i]["url"] + "]")
+            return 0
+    except Exception as e:
+        print(traceback.format_exc())
+        print("Error [" + url[i]["url"] + "] => " + str(e))
+        return 0
+
+    return 0
+
+
+def generateUrl(start):
+    global url_global
+
+    url_global = []
+    now = datetime.datetime.now()
+    year = int(now.year)
+    kl = [8, 40, 41, 42, 64, 91, 102, 103]
+
+    i = 2018
+    while i <= year:
+
+        j = 0  # j = 0 => html, j = 1 => htm
+        while j < 2:
+
+            js = ""
+            if j == 0:
+                js = "html"
+            else:
+                js = "htm"
+
+            k = 0
+            while k < len(kl):
+                ks = (str(kl[k])).zfill(3)
+                ks2 = str(i) + "_" + "20" + str(ks) + "_"
+                single = start + "/" + ks2 + js + "/" + ks2 + "generale.html"
+
+                elem = {"url": single, "year": i}
+                url_global.append(elem)
+
+                k = k + 1
+
+            j = j + 1
+
+        i += 1
+
+    pass
+
+
+def write_html(html, base_output):
+    path = base_output + "/index.html"
+    with open(path, "w") as file:
+        file.write(str(html))
+    pass
+
+
+def write_index(index_links, base_output):
+    #     elem = {"url": url[i]["url"], "index": folder, "folder": folder_first, "year": url[i]["year"]}
+    index_links.sort(key=lambda x: x["year"], reverse=True)
+
+    a = 0
+    a = a + 1
+
+    html = "<html>\n"
+    html += "<head>\n"
+    html += "<meta charset='UTF-8'>\n"
+    html += "<style> li { padding:0.5rem; } </style>\n"
+    html += "</head>\n"
+    html += "<body style='padding:1rem;'>\n"
+    html += "<h1>\n"
+    html += "Graduatorie\n"
+    html += "</h1>\n"
+    html += "<div>\n"
+    html += "<ul>\n"
+    for item in index_links:
+        html += "<li>\n"
+        link = "." + item["path"]
+        html += "<a href='" + link + "'>\n"
+        html += str(item["year"]) + " - " + str(item["corso"]) + " " + str(item["fase"]) + "\n"
+        html += "</a>\n"
+        html += "</li>\n"
+        pass
+    html += "</ul>\n"
+    html += "</div>\n"
+    html += "<h4>\n"
+    html += "Sito by "
+    html += "<a href='./../'>"
+    html += "PoliNetwork"
+    html += "</a>\n"
+    html += "</h4>\n"
+    html += "</body>\n</html>\n"
+
+    write_html(html, base_output)
+
+    pass
+
+
 # main
 if __name__ == '__main__':
-    start = "http://www.risultati-ammissione.polimi.it"
 
-    url = [
-        "http://www.risultati-ammissione.polimi.it/2020_20040_html/2020_20040_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20041_html/2020_20041_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20042_html/2020_20042_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20008_html/2020_20008_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20064_html/2020_20064_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20102_html/2020_20102_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20103_html/2020_20103_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20091_html/2020_20091_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2019_20064_html/2019_20064_sotto_indice.html",
-        "http://www.risultati-ammissione.polimi.it/2019_20102_html/2019_20102_sotto_indice.html",
-        "http://www.risultati-ammissione.polimi.it/2019_20103_html/2019_20103_sotto_indice.html",
-        "http://www.risultati-ammissione.polimi.it/2019_20091_html/2019_20091_sotto_indice.html",
-        "http://www.risultati-ammissione.polimi.it/2019_20008_html/2019_20008_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2021_20040_htm/2021_20040_sotto_indice.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20041_html/2020_20041_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2020_20042_html/2020_20042_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2021_20064_html/2021_20064_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2021_20102_html/2021_20102_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2021_20103_html/2021_20103_generale.html",
-        "http://www.risultati-ammissione.polimi.it/2021_20091_html/2021_20091_generale.html"
-    ]
+    global url_global
+
+    start = "http://www.risultati-ammissione.polimi.it"
+    base_output = "D:\\git\\Polimi\\polinetwork.github.io\\graduatorie"
+    start_len = len(start)
+
+    generateUrl(start)
+
+    index_links = []
 
     i = 0
-    while i < len(url):
-        to_download = []
-        success_download = 0
-        try:
-            downloadAndAddChildrenUrl(url[i], start)
-            if success_download > 0:
-                print("Done [" + url[i] + "]")
+    while i < len(url_global):
+
+        success = -1
+        folder, folder_first = directoryOutput(url_global[i]["url"], base_output, start_len, return_first_folder=True)
+        if os.path.isdir(folder):
+            files = os.listdir(folder)
+            if files is None or len(files) == 0:
+                success = executeDownload(url_global, i, start, base_output, only_first=False)
             else:
-                print("Error [" + url[i] + "]")
-        except Exception as e:
-            print(traceback.format_exc())
-            print("Error [" + url[i] + "] => " + str(e))
+                print("Already done [" + url_global[i]["url"] + "]")
+                executeDownload(url_global, i, start, base_output, only_first=True)
+                success = 2
+        else:
+            success = executeDownload(url_global, i, start, base_output, only_first=False)
+
+        if success == 1 or success == 2:
+            path = url_global[i]["url"][start_len:]
+            elem = {"url": url_global[i]["url"], "index": folder, "folder": folder_first, "year": url_global[i]["year"],
+                    "path": path, "corso": url_global[i]["corso"], "fase": url_global[i]["fase"]}
+            index_links.append(elem)
+            pass
 
         i = i + 1
 
-    print("Done [all]!")
+    print("Download done [all]!")
+
+    write_index(index_links, base_output)
+
+    exit(0)
